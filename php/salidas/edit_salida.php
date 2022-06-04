9ddbf3122a074cfb5769df866917dfa51cdbeca2
jhonan <jhonancalebm@gmail.com>
2022-06-04 09:29:55 -0500

    salidas

--- php/salidas/edit_salida.php
+++ php/salidas/edit_salida.php
@@ -22,8 +22,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../styles/style.css">
     <title>Document</title>
+    <style>
+        body{
+            background: transparent !important;
+        }
+    </style>
 </head>
 <body>
+    <h3>Editar Salida</h3>
     <form action="#" method="post" id="form-add">
         <div class="mb-3">
 		    <label for="codigo">Id. Salida</label>
--- php/salidas/salidas.php
+++ php/salidas/salidas.php
@@ -9,43 +9,54 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Salidas</title>
     <style>
-        body{position: relative;}
-        button{
-            position: absolute;
-            right: 30px;
+        body{
+            display:flex;
+            flex-direction:column;
+            align-items: center;
+            padding: 60px 30px;
+        }
+        .container-general{
+            position: relative;
+            display:flex;
+            align-items: center;
+            gap:50px;
+            width: 100%;
         }
         input[type="submit"]{
             width: 88%;
         }
-        form{
-            height: 0;
+        .content-form{
+            height: auto;
             overflow: hidden;
-            width: 655px;
+            width: 590px;
             border-radius:5px;
             display:flex;
             flex-wrap:wrap;
             justify-content: center;
             align-items: center;
-            background: #F0EFEF;
+            border: 3px solid #F0EFEF;
             transition:all 0.5s;
-            margin: 50px auto 0 auto ;
+            padding: 20px 15px;
 
         }
-        form.active{
+        .content-form form{
+            width: 98%;
             height: auto;
-            padding: 30px;
-            border:2px solid #DBDFEE;
-            margin: 50px auto ;
+            display:flex;
+            flex-wrap:wrap; 
+            justify-content: center;
+            align-items: center;
+            gap:10px;
         }
         form .mb-3{
-            width: 15em;
-            margin: 0 20px;
+            width: 21em;
+            margin-bottom:0 !important;
         }
-        td{
+        th{
             padding: 15px !important;
         }
         .container-iframe{
-            position: absolute;
+            position: fixed;
             width: 100%;
             height: 100vh;
             top: 0;
@@ -61,121 +72,135 @@
         .container-iframe iframe{
             position: absolute;
             width: 455px;
-            height: 595px;
-            background: white;
+            height: 615px;
+            background: rgba(255, 255,255,0.9);
             border-radius: 5px;
             padding: 15px 20px;
             border:1px solid #44DB6C;
         }
+        @media screen and (max-width:1516px){
+            .container-general{
+                flex-direction:column;
+            }
+            form .mb-3{
+                width: 10em;
+            }
+        }
     </style>
 </head>
 <body>
-    <button class="btn btn-info" id="btn-add"><i class="fa-solid fa-circle-plus"></i> Agregar Salida</button>
-    <form action="salidas.php" method="post" id="form-add">
-        <div class="mb-3">
-		    <label for="codigo">Id. Salida</label>
-		    <input type="text" class="form-control" name="tx_salida" maxlength="4" minlength="4" onkeypress="valide(event)" required/>
-		</div>	
-		<div class="mb-3">
-		    <label for="codigo">Origen</label>
-		    <input type="text" class="form-control" name="tx_origen" onkeyup="javascript:this.value=this.value.toUpperCase();" required/>
-		</div>	
-		<div class="mb-3">
-		    <label for="codigo">Destino</label>
-		    <input type="text" class="form-control" name="tx_destino" onkeyup="javascript:this.value=this.value.toUpperCase();" required/>
-		</div>	
-		<div class="mb-3">
-		    <label for="codigo">Fecha</label>
-		    <input type="date" class="form-control" name="tx_fecha" required/>
-		</div>	
-		<div class="mb-3">
-		    <label for="codigo">Hora</label>
-		    <input type="time" class="form-control" name="tx_hora" required/>
-		</div>	
-		<div class="mb-3">
-		    <label for="codigo">Monto S/</label>
-		    <input type="text" class="form-control" name="tx_monto" id="s3" onkeypress="valide(event)" required />
-		</div>	
-        <?php
-            include "../conexion.php";
-            if(!empty($_POST['tx_salida'])=="")
-            {}
-            else
-            {
-                $salida=$_POST["tx_salida"];
-                $origen=$_POST["tx_origen"];
-                $destino=$_POST["tx_destino"];
-                $fecha=$_POST["tx_fecha"];
-                $hora=$_POST["tx_hora"];
-                $monto=$_POST["tx_monto"];
-                //insertar salida
-                $sql="INSERT INTO salidas VALUES($salida,'$fecha','$hora','$origen','$destino',$monto)";
-                $sql_re=$conexion->query($sql);
-                //creando asientos de la salida
-                $i=1;
-                while($i<=36){
-                    $sql="INSERT INTO asientos(id_salida,n_asiento,estado) VALUES($salida,'$i','DISPONIBLE')";
-                    $sql_re=$conexion->query($sql);
-                    $i++;
-                }
-                
-                /* try{
-                    echo'
-                    <div class="alert alert-success" role="alert">
-                    <i>Listo.</i> Los datos se insertaron correctamente.
-                    </div> 
-                    ';
-                }
-                catch (Exception $e){
-                    echo'
-                    <div class="alert alert-danger" role="alert">
-                    <i>Error.</i> Los datos se no se insertaron.
-                    </div> 
-                    ';
-                }  */
-            }  
-        ?>
-        <input type="submit" value="Agregar" class="btn btn-primary">
-    </form>
-    <table class="table table-striped text-center">
-        <thead>
-            <tr>
-                <th scope="col">ID SALIDA</th>
-                <th scope="col">MONTO</th>
-                <th scope="col">FECHA</th>
-                <th scope="col">HORA</th>
-                <th scope="col">ORIGEN</th>
-                <th scope="col">DESTINO</th>
-                <th scope="col">OPCIONES</th>
-            </tr>
-        </thead>
-        <?php
-            $res="Select * from salidas";
-        	$vres = $conexion->query($res);
-        	while ($row=$vres->fetch_array()){	  
-                $ids = $row['id_salida']; 
-                $mon = $row['monto']; 
-                $fecha = $row['fecha']; 
-                $hora = $row['hora']; 
-                $origen = $row['origen']; 
-                $destino = $row['destino']; 
-        		echo '
-                    <tr>
-                        <td><a href="control.php?salida='.$ids.'">'.$ids.'</a></td>
-                        <td>'.$mon.'</td>
-                        <td>'.$fecha.'</td>
-                        <td>'.$hora.'</td>
-                        <td>'.$origen.'</td>
-                        <td>'.$destino.'</td>
-                        <td>
-                            <a href="eli_salida.php?salida='.$ids.'" class="btn btn-danger" title="Eliminar salida"><i class="fa-solid fa-trash"></i></a>
-                            <a target="iframe-edit" onclick="mostrar_iframe();" href="edit_salida.php?salida='.$ids.'" class="btn btn-primary" title="Editar salida"><i class="fa-solid fa-pen-to-square"></i></a>
-                        </td>
-                    </tr>
-                '; 		
-            }	
-        ?>
-    </table>
+    <h1>Gestión de Salidas</h1><br>
+    <div class="container-general">
+        <div class="content-form">
+            <h3>Agregar Salidas</h3>
+            <form action="salidas.php" method="post" id="form-add">
+                <div class="mb-3">
+                    <label for="codigo">Id. Salida</label>
+                    <input type="text" class="form-control" name="tx_salida" maxlength="4" minlength="4" onkeypress="valide(event)" required/>
+                </div>	
+                <div class="mb-3">
+                    <label for="codigo">Origen</label>
+                    <input type="text" class="form-control" name="tx_origen" onkeyup="javascript:this.value=this.value.toUpperCase();" required/>
+                </div>	
+                <div class="mb-3">
+                    <label for="codigo">Destino</label>
+                    <input type="text" class="form-control" name="tx_destino" onkeyup="javascript:this.value=this.value.toUpperCase();" required/>
+                </div>	
+                <div class="mb-3">
+                    <label for="codigo">Fecha</label>
+                    <input type="date" class="form-control" name="tx_fecha" required/>
+                </div>	
+                <div class="mb-3">
+                    <label for="codigo">Hora</label>
+                    <input type="time" class="form-control" name="tx_hora" required/>
+                </div>	
+                <div class="mb-3">
+                    <label for="codigo">Monto S/</label>
+                    <input type="text" class="form-control" name="tx_monto" id="s3" onkeypress="valide(event)" required />
+                </div>	
+                <input type="submit" value="Agregar" class="btn btn-primary">
+                <br><br>
+                <?php
+                    include "../conexion.php";
+                    if(!empty($_POST['tx_salida'])=="")
+                    {}
+                    else
+                    {
+                        $salida=$_POST["tx_salida"];
+                        $origen=$_POST["tx_origen"];
+                        $destino=$_POST["tx_destino"];
+                        $fecha=$_POST["tx_fecha"];
+                        $hora=$_POST["tx_hora"];
+                        $monto=$_POST["tx_monto"];
+                        //insertar salida
+                        $sql="INSERT INTO salidas VALUES($salida,'$fecha','$hora','$origen','$destino',$monto)";
+                        $sql_re=$conexion->query($sql);
+                        //creando asientos de la salida
+                        $i=1;
+                        while($i<=36){
+                            $sql="INSERT INTO asientos(id_salida,n_asiento,estado) VALUES($salida,'$i','DISPONIBLE')";
+                            $sql_re=$conexion->query($sql);
+                            $i++;
+                        }  
+                        try{
+                            echo'
+                            <div class="alert alert-success" role="alert">
+                            <i>Listo.</i> Los datos se insertaron correctamente.
+                            </div> 
+                            ';
+                        }
+                        catch (Exception $e){
+                            echo'
+                            <div class="alert alert-danger" role="alert">
+                            <i>Error.</i> Los datos se no se insertaron.
+                            </div> 
+                            ';
+                        } 
+                    }  
+                ?>
+            </form>
+        </div>
+        <table class="table table-striped text-center">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">ID SALIDA</th>
+                    <th scope="col">MONTO</th>
+                    <th scope="col">FECHA</th>
+                    <th scope="col">HORA</th>
+                    <th scope="col">ORIGEN</th>
+                    <th scope="col">DESTINO</th>
+                    <th scope="col">OPCIONES</th>
+                </tr>
+            </thead>
+            <?php
+                $res="Select * from salidas";
+            	$vres = $conexion->query($res);
+            	while ($row=$vres->fetch_array()){	  
+                    $ids = $row['id_salida']; 
+                    $mon = $row['monto']; 
+                    $fecha = $row['fecha']; 
+                    $hora = $row['hora']; 
+                    $origen = $row['origen']; 
+                    $destino = $row['destino']; 
+            		echo '
+                        <tr>
+                            <td><a href="control.php?salida='.$ids.'">'.$ids.'</a></td>
+                            <td>'.$mon.'</td>
+                            <td>'.$fecha.'</td>
+                            <td>'.$hora.'</td>
+                            <td>'.$origen.'</td>
+                            <td>'.$destino.'</td>
+                            <td>
+                                <a href="eli_salida.php?salida='.$ids.'" class="btn btn-danger" title="Eliminar salida"><i class="fa-solid fa-trash"></i></a>
+                                <a target="iframe-edit" onclick="mostrar_iframe();" href="edit_salida.php?salida='.$ids.'" class="btn btn-primary" title="Editar salida"><i class="fa-solid fa-pen-to-square"></i></a>
+                            </td>
+                        </tr>
+                    '; 		
+                }	
+            ?>
+        </table>
+    </div>
+    
     <div class="container-iframe">
         <iframe src="" name="iframe-edit" frameborder="0"></iframe>
     </div>
@@ -184,12 +209,12 @@
             if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;
         }
         //archivo salidas.php script para aparecer el form de agregar 
-        const btn_add=document.querySelector("#btn-add");
+        /* const btn_add=document.querySelector("#btn-add");
         let form=document.querySelector("#form-add");
         function show(){
             form.classList.toggle('active');
         }
-        btn_add.onclick=show;
+        btn_add.onclick=show; */
         //funcion parav aparecer iframe de editar salida 
         const btn_show_iframe_edit=document.querySelector(".btn-cerrar");
         let iframe_edit=document.querySelector(".container-iframe");
